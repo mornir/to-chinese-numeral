a055f3d6e5a34bf590540b139e1db6a0ff9be623
mornir <jeromepott@hotmail.com>
2020-11-24 18:24:44 +0100

    Comment out advance tests

--- kata.test.js
+++ kata.test.js
@@ -16,7 +16,7 @@ describe("Chinese Numerals", function () {
     expect(toChineseNumeral(111)).toBe("一百一十一")
     expect(toChineseNumeral(1000)).toBe("一千")
     expect(toChineseNumeral(10000)).toBe("一万")
-    /* expect(toChineseNumeral(10006)).toBe("一万零六") */
-    /* expect(toChineseNumeral(10306.005)).toBe("一万零三百零六点零零五") */
+    /*  expect(toChineseNumeral(10006)).toBe("一万零六")
+    expect(toChineseNumeral(10306.005)).toBe("一万零三百零六点零零五") */
   })
 })
