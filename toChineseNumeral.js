8d0e2b2b48580af840ffa87052dd9dd618a95986
mornir <jeromepott@hotmail.com>
2020-12-12 20:52:47 +0100

    Refactor dizaines

--- toChineseNumeral.js
+++ toChineseNumeral.js
@@ -37,32 +37,19 @@ function toChineseNumeral(num) {
   }
 
   function generateNumber(nums) {
-    function handleZeroes(chineseNumeral) {
+    function handleEdgecases(chineseNumeral) {
       if (chineseNumeral === "零") {
         return "零"
       }
 
       const mergeMultipleZeroes = /零+/g
       const removeTrailingZero = /零$/g
+      const removeLeadingTenth = /^一十/g
 
       return chineseNumeral
         .replace(mergeMultipleZeroes, "零")
         .replace(removeTrailingZero, "")
-    }
-
-    function dizaines([num1, num2]) {
-      let position1 = ""
-      if (num1 === "0") {
-        position1 = "零"
-      } else if (num1 !== "1" || num > 20) {
-        position1 = numerals[num1]
-      }
-
-      let position2 = num1 !== "0" ? numerals[10] : ""
-
-      let position3 = num2 !== "0" ? numerals[num2] : ""
-
-      return position1 + position2 + position3
+        .replace(removeLeadingTenth, "十")
     }
 
     function tenX([num1, ...rest]) {
@@ -76,8 +63,9 @@ function toChineseNumeral(num) {
         return numerals[0] + tenX(rest)
       }
 
+      // dizaines
       if (rest.length === 1) {
-        return dizaines([num1, ...rest])
+        tenth = 10
       }
 
       // centaines
@@ -98,7 +86,7 @@ function toChineseNumeral(num) {
       return numerals[num1] + numerals[tenth] + tenX(rest)
     }
 
-    num = handleZeroes(tenX(nums))
+    num = handleEdgecases(tenX(nums))
 
     if (isNeg) {
       num = numerals["-"] + num
@@ -110,7 +98,5 @@ function toChineseNumeral(num) {
     return decimals.map((number) => numerals[number]).join("")
   }
 }
-
-//console.log(toChineseNumeral(1000))
 
 module.exports = toChineseNumeral
